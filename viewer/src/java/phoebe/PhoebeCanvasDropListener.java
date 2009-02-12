c220cd26c56cc8d45f0de80b28d91b309fbc1839
konstantin.vandev <konstantin.vandev@43784a7f-e236-0410-9472-c78ae7dffccf>
2009-02-12 15:31:19 +0000

    ok
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@484 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/src/java/phoebe/PhoebeCanvasDropListener.java
+++ viewer/src/java/phoebe/PhoebeCanvasDropListener.java
@@ -1,30 +1,6 @@
-/*
- * Created on Jun 13, 2005
- *
- * an interface for responding to PhoebeCanvasDropEvents.  
- */
-package phoebe;
-
-import java.awt.event.*;
+/* * Created on Jun 13, 2005 * * an interface for responding to PhoebeCanvasDropEvents.   */package phoebe;
 import java.util.*;
-import java.awt.*;
-import java.awt.datatransfer.Transferable;
-
-/**
- * @author Allan Kuchinsky
- *
- */
-
-/**
- * an interface for responding to PhoebeCanvasDropEvents.  
- */
-public interface PhoebeCanvasDropListener extends EventListener {
-
-
-	/**
-	 * method for responding to a drop
-	 * @param event the PhoebeCanvasDropEvent
-	 */
-	void itemDropped(PhoebeCanvasDropEvent event);
-
+/** * @author Allan Kuchinsky * */
+/** * an interface for responding to PhoebeCanvasDropEvents.   */public interface PhoebeCanvasDropListener extends EventListener {
+     /**      * method for responding to a drop	 * @param event the PhoebeCanvasDropEvent	 */	void itemDropped(PhoebeCanvasDropEvent event);
 }
