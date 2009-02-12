45602404d0e8eb3abd7f9753aad713c29cae624c
konstantin.vandev <konstantin.vandev@43784a7f-e236-0410-9472-c78ae7dffccf>
2009-02-12 13:28:34 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@481 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/src/java/phoebe/event/GraphPerspectiveChangeListener.java
+++
@@ -1,5 +0,0 @@
-package phoebe.event;
-import java.util.EventListener;
-public interface GraphPerspectiveChangeListener extends EventListener {
-  /**   * Invoked when a GraphPerspective to which this   * GraphPerspectiveChangeListener listens changes.   */ public void graphPerspectiveChanged ( GraphPerspectiveChangeEvent event );
-} // interface GraphPerspectiveChangeListener
