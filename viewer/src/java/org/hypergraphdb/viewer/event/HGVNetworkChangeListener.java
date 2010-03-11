25add46198e92674ac49bf7d6f53ec07c5e6cd09
konstantin.vandev <konstantin.vandev@43784a7f-e236-0410-9472-c78ae7dffccf>
2010-03-11 16:56:49 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@891 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/src/java/org/hypergraphdb/viewer/event/HGVNetworkChangeListener.java
+++
@@ -1,5 +0,0 @@
-package org.hypergraphdb.viewer.event;
-import java.util.EventListener;
-public interface HGVNetworkChangeListener extends EventListener {
-   public void networkChanged ( HGVNetworkChangeEvent event );
-} 
