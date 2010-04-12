c8587c491e9a2eddd0bde61a64d3c9faee4d9738
konstantin.vandev <konstantin.vandev@43784a7f-e236-0410-9472-c78ae7dffccf>
2010-04-12 17:30:04 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@962 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/run.cmd
+++ viewer/run.cmd
@@ -1,11 +1,13 @@
 @echo off
 set JAVA_HOME = C:\Program Files (x86)\Java\jdk1.6.0_10
+set HGDB_HOME = F:\kosta\ticl\new_hg\core
 set VIEWER_HOME=%CD%
 
 set VIEWER_CLASSPATH="%VIEWER_HOME%/hgdbviewer.jar"
+set HGDB_NATIVE=%HGDB_HOME%/native/windows
 set JAVA_EXEC="%JAVA_HOME%/bin/java"
 
-echo %PATH%
+set PATH=%HGDB_NATIVE%;%PATH%
 
 set LIB_JARS=
 echo set LIB_JARS=%%~1;%%LIB_JARS%%>append.bat
@@ -13,7 +15,7 @@ dir /s/b jars\*.jar > tmpList.txt
 FOR /F "usebackq tokens=1* delims=" %%i IN (tmpList.txt) do (call append.bat "%%i")
 del append.bat
 del tmpList.txt
-set VIEWER_CLASSPATH=%LIB_JARS%;%VIEWER_CLASSPATH%
+set VIEWER_CLASSPATH=%LIB_JARS%;HGDB_HOME;%VIEWER_CLASSPATH%
 echo %VIEWER_CLASSPATH%
 
-%JAVA_EXEC% -cp %VIEWER_CLASSPATH% org.hypergraphdb.viewer.HGVDesktop
+%JAVA_EXEC% -cp %VIEWER_CLASSPATH% -Djava.library.path=%HGDB_NATIVE%  org.hypergraphdb.viewer.HGVDesktop
