14a20c526ce212819fc6d5a82fb8495f0b7bba8c
konstantin.vandev <konstantin.vandev@43784a7f-e236-0410-9472-c78ae7dffccf>
2010-04-15 16:56:00 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@977 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/run.sh
+++ viewer/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#Uncomment and set appropriately those 2 vars
+#JAVA_HOME=
+#HGDB_HOME=
+ 
 # OS specific support.  $var _must_ be set to either true or false.
 pathsep=':'
 systemname='windows'
@@ -25,15 +29,22 @@ JAVA_EXEC=java
 
 VIEWER_CLASSPATH="$VIEWER_HOME/hgdbviewer.jar"
 
+cd $HGDB_HOME
+for f in *.jar; do
+  VIEWER_CLASSPATH="$VIEWER_CLASSPATH$pathsep$f"
+done;
+cd $VIEWER_BIN_DIR
+
 for f in jars/*.jar; do
   VIEWER_CLASSPATH="$VIEWER_CLASSPATH$pathsep$f"
 done;
 
-VIEWER_NATIVE=""
+
+ HGDB_NATIVE=$HGDB_HOME$\native\windows
 
 if $cygwin; then
   [ -n "$VIEWER_HOME" ] && VIEWER_CLASSPATH=`cygpath --absolute --path --windows "$VIEWER_CLASSPATH"`
-  [ -n "$VIEWER_NATIVE" ] && VIEWER_NATIVE=`cygpath --path --windows "$VIEWER_NATIVE"`
+  [ -n "$HGDB_NATIVE" ] && HGDB_NATIVE=`cygpath --path --windows "$HGDB_NATIVE"`
 fi
 
-exec $JAVA_EXEC -cp $VIEWER_CLASSPATH  -Djava.library.path=$VIEWER_NATIVE org.hypergraphdb.viewer.HGVDesktop
+exec $JAVA_EXEC -cp $VIEWER_CLASSPATH  -Djava.library.path=$HGDB_NATIVE org.hypergraphdb.viewer.HGVDesktop
