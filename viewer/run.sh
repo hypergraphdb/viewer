9584ecf76237acc9ea77287f018d7bb912f0155c
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2010-05-03 16:56:38 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1025 43784a7f-e236-0410-9472-c78ae7dffccf

--- viewer/run.sh
+++ viewer/run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#Uncomment and set appropriately those 2 vars
-#JAVA_HOME=
-#HGDB_HOME=
- 
+#Uncomment and set appropriately
+#HGDB_HOME="/usr/local/hypergraphdb"
+
 # OS specific support.  $var _must_ be set to either true or false.
 pathsep=':'
 systemname='windows'
@@ -19,6 +18,9 @@ CYGWIN*)
   ;;
 esac
 
+if [ `uname -m` = 'x86_64' ]; then
+  systemarch='/x86_64'
+fi
 
 VIEWER_BIN_DIR=`dirname $0`
 cd $VIEWER_BIN_DIR
@@ -28,23 +30,28 @@ echo "Using VIEWER  home directory '$VIEWER_HOME'"
 JAVA_EXEC=java
 
 VIEWER_CLASSPATH="$VIEWER_HOME/hgdbviewer.jar"
+VIEWER_CLASSPATH="$VIEWER_CLASSPATH$pathsep$HGDB_HOME/hgdbfull.jar"
 
-cd $HGDB_HOME
-for f in *.jar; do
+for f in $HGDB_HOME/jars/*.jar; do
   VIEWER_CLASSPATH="$VIEWER_CLASSPATH$pathsep$f"
 done;
-cd $VIEWER_BIN_DIR
+
+cd $VIEWER_HOME
 
 for f in jars/*.jar; do
+  echo "F is $f"
   VIEWER_CLASSPATH="$VIEWER_CLASSPATH$pathsep$f"
 done;
 
 
- HGDB_NATIVE=$HGDB_HOME$\native\windows
+HGDB_NATIVE=$HGDB_HOME/native/$systemarch
+
+echo "Directory containing native libs: $HGDB_NATIVE"
 
 if $cygwin; then
   [ -n "$VIEWER_HOME" ] && VIEWER_CLASSPATH=`cygpath --absolute --path --windows "$VIEWER_CLASSPATH"`
   [ -n "$HGDB_NATIVE" ] && HGDB_NATIVE=`cygpath --path --windows "$HGDB_NATIVE"`
 fi
 
+echo "Using classpath: $VIEWER_CLASSPATH"
 exec $JAVA_EXEC -cp $VIEWER_CLASSPATH  -Djava.library.path=$HGDB_NATIVE org.hypergraphdb.viewer.HGVDesktop
